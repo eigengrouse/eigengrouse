2416297a1ed2bb08b91df110a1f54e02fd43f063
Adam <adam.belshaw@hicomply.com>
2026-02-01 14:52:08 +0000

    install astro-mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+import mermaid from 'astro-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.eigengrouse.com',
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    mermaid({
+      theme: 'forest',
+      autoTheme: true
+    })
+  ],
 })
